df6f80cbc07c38fb6b4b52bba40c743149cddbe9
PaigeSylvan <paigesylvan@gmail.com>
2025-12-14 21:03:56 -0600

    updated

--- components/Project1/9-testing.tsx
+++ components/Project1/9-testing.tsx
@@ -6,7 +6,7 @@ import SectionHeader from "../SectionHeader";
 export default function Testing() {
   return (
     <section className="flex flex-col items-center justify-center px-4 md:px-4 text-white py-8 md:py-10 mt-20 md:mt-48 mb-8 lg:mb-0">
-      <div className="max-w-[760px] w-full text-center mx-auto">
+      <div className="max-w-[900px] w-full text-center mx-auto">
 
         <SectionHeader
           kicker="TESTING USABILITY"
