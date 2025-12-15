9ba8cfd414d7cc100a32127e34b24a25a42d8393
PaigeSylvan <paigesylvan@gmail.com>
2025-12-14 22:13:58 -0600

    updated

--- components/Project1/8-protoype.tsx
+++ components/Project1/8-protoype.tsx
@@ -5,7 +5,7 @@ import SectionHeader from "../SectionHeader";
 
 export default function Prototype() {
   return (
-    <section className="flex flex-col justify-center text-white mt-24 md:mt-0 max-w-[1000px]">
+    <section className="flex flex-col justify-center text-white mt-24 md:mt-0 max-w-[900px]">
 
         <div className="">
           <SectionHeader
