384baa2683c289141168e086e2c6e1a42e9ffdef
PaigeSylvan <paigesylvan@gmail.com>
2025-12-14 20:02:10 -0600

    updated

--- components/About/1-hero.tsx
+++ components/About/1-hero.tsx
@@ -18,7 +18,7 @@ const SKILLS: SkillIcon[] = [
   { src: "/images/skills/7.png", alt: "Next.js", className: "right-[12%] top-[12%] lg:right-[11%] lg:top-[9%]" },
   { src: "/images/skills/14.png", alt: "GitHub", className: "right-[-5%] top-[23%] lg:right-[-3%] lg:top-[15%]" },
 
-  { src: "/images/skills/4.png", alt: "Google Analytics", className: "left-[-18%] top-[30%] lg:left-[-8%] lg:top-[26%]" },
+  { src: "/images/skills/4.png", alt: "Google Analytics", className: "left-[-17%] top-[27%] lg:left-[-8%] lg:top-[26%]" },
   { src: "/images/skills/10.png", alt: "Tailwind.css", className: "left-[0%] top-[40%] lg:left-[4%] lg:top-[40%]" },
   { src: "/images/skills/9.png", alt: "Sass.css", className: "left-[-5%] top-[52%] lg:left-[0%] lg:top-[58%]" },
 
