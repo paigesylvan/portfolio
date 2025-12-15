a513958886f1421fc5ec272a973cd3fb7ec09cfd
PaigeSylvan <paigesylvan@gmail.com>
2025-12-14 23:46:04 -0600

    updated

--- components/About/2-timeline.tsx
+++ components/About/2-timeline.tsx
@@ -228,7 +228,7 @@ export default function TimelineAbout() {
   return (
     <section className="relative isolate bg-black text-white pb-28 md:pb-36 px-4 md:px-6">
       <div className="mx-auto w-full max-w-[1000px]]">
-        <div className="text-left relative">
+        <div className="text-left relative lg:ml-[400px]">
           <p className="text-[11px] tracking-[0.22em] text-white/60">
             THE PATH SO FAR
           </p>
