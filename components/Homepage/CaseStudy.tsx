fce14278cb8ed157000a4cb2d13b6a13161f9e0c
PaigeSylvan <paigesylvan@gmail.com>
2025-12-15 15:59:07 -0600

    updated

--- components/Homepage/CaseStudy.tsx
+++ components/Homepage/CaseStudy.tsx
@@ -103,7 +103,12 @@ export default function CaseStudies() {
       "
     >
       {/* aurora */}
-      <div className="    cs-aurora-bottom pointer-events-none absolute bottom-0 h-[32vh] w-screen z-0 opacity-30" />
+      <div className="    cs-aurora-bottom
+    pointer-events-none
+    absolute inset-x-0 bottom-0
+    h-[32vh]
+    z-0
+    opacity-60" />
 
       {/* ✅ same container rules as Skills */}
       <div className="relative z-10 mx-auto w-full max-w-[1100px] px-6">
