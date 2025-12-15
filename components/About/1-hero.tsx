584b95012c08d9b2cfa8253a96a64bfad8f23191
PaigeSylvan <paigesylvan@gmail.com>
2025-12-14 19:52:35 -0600

    updated

--- components/About/1-hero.tsx
+++ components/About/1-hero.tsx
@@ -14,8 +14,8 @@ const SKILLS: SkillIcon[] = [
   { src: "/images/skills/1.png", alt: "Canva", className: "left-[29%] top-[10%] lg:left-[22%] lg:top-[8%]" },
   { src: "/images/skills/3.png", alt: "Adobe AE", className: "left-[33%] top-[23%] lg:left-[36%] lg:top-[17%]" },
   { src: "/images/skills/5.png", alt: "Figma", className: "left-1/2 -translate-x-1/2 top-[5%] lg:top-[6%]" },
-  { src: "/images/skills/13.png", alt: "VS Code", className: "right-[33%] top-[24%] lg:right-[28%] lg:top-[17%]" },
-  { src: "/images/skills/7.png", alt: "Next.js", className: "right-[18%] top-[12%] lg:right-[11%] lg:top-[9%]" },
+  { src: "/images/skills/13.png", alt: "VS Code", className: "right-[22%] top-[24%] lg:right-[28%] lg:top-[17%]" },
+  { src: "/images/skills/7.png", alt: "Next.js", className: "right-[12%] top-[12%] lg:right-[11%] lg:top-[9%]" },
   { src: "/images/skills/14.png", alt: "GitHub", className: "right-[-5%] top-[23%] lg:right-[-3%] lg:top-[15%]" },
 
   { src: "/images/skills/4.png", alt: "Google Analytics", className: "left-[-10%] top-[25%] lg:left-[-8%] lg:top-[26%]" },
