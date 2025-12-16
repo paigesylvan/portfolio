c6c5a50a88a28c382dc4265371dc444fdf3360de
PaigeSylvan <paigesylvan@gmail.com>
2025-12-15 20:07:05 -0600

    updated

--- components/Project2/1-Hero.tsx
+++ components/Project2/1-Hero.tsx
@@ -5,9 +5,9 @@ import SectionHeader from "../SectionHeader";
 
 export default function HeroOverviewCamping() {
   return (
-    <section className="full-bleed bg-black text-white py-10 md:py-14 lg:mt-[50px] px-0">
+    <section className="full-bleed bg-black text-white py-10 md:py-14 lg:mt-[50px]">
       {/* ✅ Whole section constrained */}
-      <div className="mx-auto w-full max-w-[4000px] px-4 md:px-0">
+      <div className="mx-auto w-full  px-4 md:px-0">
         <SectionHeader
           kicker="RESPONSIVE WEBSITE DESIGN"
           title="How Thoughtful UX Design Increased Engagement for First-Time Campers"
