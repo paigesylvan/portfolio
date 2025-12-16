12bcc8fb2d1c921adb31fc1e5cf074ef21b96f24
PaigeSylvan <paigesylvan@gmail.com>
2025-12-15 21:27:45 -0600

    updated

--- components/Project1/3-research.tsx
+++ components/Project1/3-research.tsx
@@ -110,7 +110,7 @@ export default function ResearchInsights() {
 
         {/* Donuts */}
         <div className="mb-8">
-          <div className="mx-auto grid w-fit grid-cols-3 gap-x-6 lg:gap-x-8 py-6">
+          <div className="mx-auto grid w-fit grid-cols-3 gap-x-6 lg:gap-x-24 py-6">
             <Donut
               percent={67}
               captionTop="of U.S. households"
